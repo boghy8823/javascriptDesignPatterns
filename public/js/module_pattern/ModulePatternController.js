3396da98994ce13b809e6b73143a08c58f27674b
unknown <bodu@WRKMTSITEBODU.Betsson.local>
2015-12-09 19:36:44 +0100

    Added sandboxed iframe to run js snippets inside

--- public/js/module_pattern/ModulePatternController.js
+++ public/js/module_pattern/ModulePatternController.js
@@ -1,7 +1,43 @@
+
+
 (function(angular){
 	angular
 	.module('jsPatternsApp.modulePattern')
-	.controller('ModulePatternController', function( $scope ){
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+	.controller('ModulePatternController', ['$scope', '$sce', function( $scope, $sce ){
 		console.log("This is the module pattern controller");
-	});
+      
+      var frame = document.getElementById('sandboxed');
+      var code = document.getElementById('code').value;
+
+console.log("frame", frame);
+console.log("code",code);
+    var iframeMarkup = 
+        "<html>" +
+          "<head>" +
+            "<title>Evalboxs Frame</title>" +
+            "<script>" +
+              "console.log('Script');";
+              "window.addEventListener('message', function (e) {" +
+                  "return;" +
+
+                "var mainWindow = e.source;" +
+                "var result = '';" +
+                "try {" +
+                  "result = eval(e.data);" +
+                  "console.log('result:',result);" +
+                "} catch (e) {" +
+                  "result = 'eval() threw an exception.';" +
+                "}" +
+                "mainWindow.postMessage(result, e.origin);" +
+              "});" +
+            "</script>" +
+          "</head>" +
+        "</html>"
+ 
+
+       $scope.trustedMarkup =  iframeMarkup;
+	}]);
 })(window.angular);
